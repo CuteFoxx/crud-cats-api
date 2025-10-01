243cb065cc7682a2b7b90942964d6972adb313e2
Alice <alicepolishchuk1@gmail.com>
2025-10-01 20:37:31 +0300

    Add file create/delte route, add static files directory, add cat create route

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { File } from './file.entity';
+import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  constructor(
+    @InjectRepository(File) private readonly fileRepo: Repository<File>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async create(file: Express.Multer.File): Promise<File> {
+    const fileEntity = this.fileRepo.create({
+      path: file.path,
+      filename: file.filename,
+      mimetype: file.mimetype,
+      created: new Date(),
+    });
+
+    return this.fileRepo.save(fileEntity);
+  }
+
+  async delete(fid: string): Promise<File> {
+    const file = await this.fileRepo.findOneBy({ id: parseInt(fid) });
+    if (!file) {
+      throw new NotFoundException('file not found');
+    }
+    return this.fileRepo.remove(file);
+  }
+}
